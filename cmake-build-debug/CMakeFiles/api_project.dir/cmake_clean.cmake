bfeb000094025ae9f4f731ee4dd5f2ea64616b36
Sonny <swagpino@gmail.com>
2020-07-27 18:40:35 +0200

    (ノಠ益ಠ)ノ彡┻━┻

---
+++ cmake-build-debug/CMakeFiles/api_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/api_project.dir/main.c.o"
+  "api_project"
+  "api_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/api_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    int n=0;
-    printf("Hello, World!    %d,\n", n);
+    printf("Hello, World!\n");
     return 0;
 }

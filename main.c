c9b597c08e485a99b89572d4068de72ee3fa70ed
Sonny <swagpino@gmail.com>
2020-08-08 17:37:14 +0200

    (ノಠ益ಠ)ノ彡┻━┻

--- main.c
+++ main.c
@@ -4,136 +4,179 @@
 #include "string.h"
 #include "math.h"
 
-typedef struct array
-{
+typedef struct array {
     char *data;
+    struct array *prev;
     struct array *next;
 } array;
 
-void divideind(int ind1, int ind2, char *cmd, int length);
+void divideaddr(int *ind1, int *ind2, char *cmd, int length);
 
-bool getinput(char *inputln){};
+void printstr(char **strings, int length, int addr1, int addr2);
 
-void printstr(char **strings){
+void getinput(char **strings, int addr1, int addr2);
 
-};
+void changestr(char **strings, int addr1, int addr2, int *length, int len, array **commands);
 
-void changestr(){
+void delstr();
 
-};
+void undo();
 
-void delstr(){
+void redo();
 
-};
 
-void undo(){
-
-};
-
-void redo(){
-
-};
-
-int main()
-{
+int main() {
+    array *commands;
+    commands = (array*)malloc(sizeof(array));
+    commands->next = NULL;
+    commands->prev = NULL;
+    commands->data= NULL;
+    int addr1 = 0, addr2 = 0;
     int cmdlength = 0;
-    int ind1 = 0, ind2 = 0;
-    char **strings = NULL;
+    int *ind1;
+    int *ind2;
+    int *length;
+    char **strings = (char **) calloc(1, sizeof(char *));
+    int stringslength = 1;
     char *cmd = NULL;
     char c;
     int index = 0;
-    while (true)
-    {
-        do
-        {
+    ind1 = &addr1;
+    ind2 = &addr2;
+    length = &stringslength;
+    while (true) {
+        do {
             c = getchar_unlocked();
-            index++;
-            if (cmd == NULL)
-            {
-                cmd = (char *)malloc(sizeof(char));
+            if (cmd == NULL) {
+                cmd = (char *) malloc(sizeof(char));
                 cmd[0] = c;
-            }
-            else
-            {
-                cmd = realloc(cmd, (index) * sizeof(char));
-                cmd[index - 1] = c;
+                index++;
+            } else {
+                if (index >= cmdlength) {
+                    cmd = realloc(cmd, (index + 2) * sizeof(char));
+                    cmd[index] = c;
+                    index++;
+                } else {
+                    cmd[index] = c;
+                    index++;
+                }
             }
         } while (c != '\n');
+        if (index <= cmdlength) {
+            cmd[index] = '\0';
+        }
         cmdlength = strlen(cmd);
         c = cmd[cmdlength - 2];
-        divideind(ind1, ind2, cmd, cmdlength);
-        printf("%d  %d", ind1, ind2);
-        switch (c)
-        {
-        case ('p'):
-            printstr(strings);
-            index = 0;
-            return 0;
-        case ('d'):
-            delstr();
-            printf("delete");
-            break;
-        case ('c'):
-            changestr();
-            printf("change");
-            break;
-        case ('r'):
-            redo();
-            printf("redo");
-            break;
-        case ('u'):
-            undo();
-            printf("undo");
-            break;
-        case ('q'):
-            return 0;
-        default:
-            printf("default");
-            break;
+        divideaddr(ind1, ind2, cmd, cmdlength);
+        switch (c) {
+            case ('p'):
+                printstr(strings, stringslength, addr1, addr2);
+                index = 0;
+                break;
+            case ('d'):
+                delstr();
+                printf("delete");
+                break;
+            case ('c'):
+                changestr(strings,addr1, addr2,length, stringslength, &commands);
+                printf("change");
+                break;
+            case ('r'):
+                redo();
+                printf("redo");
+                break;
+            case ('u'):
+                undo();
+                printf("undo");
+                break;
+            case ('q'):
+                return 0;
+            default:
+                printf("default");
+                break;
         }
     }
 }
 
-void divideind(int ind1, int ind2, char *cmd, int length)
-{
-    printf("tommy gay");
+void divideaddr(int *ind1, int *ind2, char *cmd, int length) {
+    int addr1 = 0, addr2 = 0;
     int num = 0;
     int check = 0;
-    int m;
     int i;
-    for (i = 0; i < length - 2; i++)
-    {
-        if (cmd[i] != ',')
-        {
+    for (i = 0; i < length - 2; i++) {
+        if (cmd[i] != ',') {
             check++;
-        }
-        else
+        } else
             break;
     }
     double k = 0;
-    for (int j = i - 1; j >= 0; j--)
-    {
-        printf("\n%c\n", cmd[j]);
+    for (int j = i - 1; j >= 0; j--) {
         num = cmd[j] - '0';
-        printf("pipo %d pipo\n", num);
-        ind1 = ind1 + (int)(num * pow(10, k));
-        printf("%d", ind1);
+        addr1 = addr1 + (int) (num * pow(10, k));
         k++;
     }
     k = 0;
-    for (int m = check; m < length - 2; m++)
-    {
-        if (cmd[m] != '\n')
-        {
-            m++;
-        }
-        else
+    int m;
+    for (m = check; m < length - 2; m++) {
+        if (cmd[m] == '\n') {
             break;
+        }
     }
-    for (int n = m - 1; n >= 0; n--)
-    {
+    for (int n = m - 1; n > check; n--) {
         num = cmd[n] - '0';
-        ind2 = ind2 + (int)(num * pow(10, k));
+        addr2 = addr2 + (int) (num * pow(10, k));
         k++;
+    }
+    *ind1 = addr1;
+    *ind2 = addr2;
+}
+
+void printstr(char **strings, int length, int addr1, int addr2) {
+    if (strings[0] == NULL) {
+        for (int i = addr1; i <= addr2; ++i) {
+            printf(".\n");
+        }
+    } else {
+        if (addr1 == 0 && addr2 == 0) {
+            printf(".\n");
+            return;
+        } else if (addr1 == 0) {
+            printf(".\n");
+            addr1++;
+        }
+        for (int i = addr1; i <= addr2; ++i) {
+            if (i > length) {
+                printf(".\n");
+            } else {
+                printf("%s", strings[i - 1]);
+            }
+        }
+    }
+}
+
+void changestr(char **strings, int addr1, int addr2, int *length, int len, array **commands) {
+    int templen=0;
+    char c;
+    char **strings1 = (char **) malloc(sizeof(char *));
+    int index = 0;
+    //receive input and store in a temp strings1
+    for (int i = 0; i <= addr2 - addr1 + 1; ++i) {
+        index = 0;
+        if (i != 0) {
+            strings1 = realloc(strings1, (i + 1) * sizeof(char *));
+        }
+        strings1[i] = (char *) malloc(sizeof(char));
+        do {
+            c = getchar_unlocked();
+            if (index != 0) {
+                strings1[i] = realloc(strings1[i], (index + 1) * sizeof(char));
+            }
+            strings1[i][index] = c;
+            index++;
+        } while (c != '\n');
+    templen++;
+    }
+    for (int j = 0; j < templen; ++j) {
+        printf("%s", strings1[j]);
     }
 }
